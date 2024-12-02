61e8ec11903051d3bb6ac0a803aadbf56bb4e9d8
Vajda Gergely <vajda.gergely@pillerkft.hu>
2024-12-02 13:28:46 +0100

    task1 completed, and project init

---
+++ 2024/task1/task1.js
@@ -0,0 +1,18 @@
+import {
+    dataLocIdsCol1,
+    dataLocIdsCol2
+} from './data.js';
+
+
+//dataLocIdsCol1
+//dataLocIdsCol2
+const col1 = dataLocIdsCol1.sort();
+const col2 = dataLocIdsCol2.sort();
+
+let sum = 0;
+
+for (let i = 0; i < col1.length; i++) {
+    sum += (Math.abs(col1[i] - col2[i]));
+}
+
+console.log(sum);
